9920e29b60a64cba3af20df136c6f80edf8fa027
Klimonov <dh-ka@bk.ru>
2017-06-28 22:06:34 +0300

    new change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-	
+	// Библиотека Fullpage
 	$('#fullpage').fullpage({
 		sectionsColor:['#363338', '#6E857D', '#756F68', '#979C93'],
 		anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
@@ -8,6 +8,33 @@ jQuery(document).ready(function() {
 		slidesNavigation: true,
 	})
 
+// Табы на главной секции
+
+	$('.wrapperSection1 div').not(':first-of-type').hide();
+
+
+	$('.tab-menu li').each(function(i) {
+
+		$(this).attr('data-tab', 'tab'+i);
+
+	});
+
+		$('.wrapperSection1 div').each(function(i) {
+
+		$(this).attr('data-tab', 'tab'+i);
+
+	});
+
+	$('.tab-menu li').on('click', function() {
+
+		var dataTab = $(this).data('tab');
+
+		$('.tab-menu li').removeClass('active');
+		$(this).addClass('active');
+
+		$('.wrapperSection1 div').fadeOut(400);
+		$('.wrapperSection1 div[data-tab='+dataTab+']').fadeIn(400);
+	});
 
 
 });
