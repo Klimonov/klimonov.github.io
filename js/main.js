c5c9ae2f465ad6c981ff698cab69d19801bab31f
Klimonov <dh-ka@bk.ru>
2017-06-28 11:37:19 +0300

    add new design

---
+++ js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+	
+	$('#fullpage').fullpage({
+		sectionsColor:['#363338', '#6E857D', '#756F68', '#979C93'],
+		anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+		menu:'#menu',
+		navigation: true,
+		slidesNavigation: true,
+	})
+
+
+
+});
